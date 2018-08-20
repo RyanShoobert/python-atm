16605839490e227c9d82d8c8d7e2cd2b24d6e65b
RyanShoobertUoB <shoobertr@gmail.com>
2018-08-20 19:49:37 +0100

    Initial Commit
    -Initial creation of files

---
+++ controller/ATM_Controller.py
@@ -0,0 +1,16 @@
+'''
+Created on 2018-08-20 (YYYY-MM-DD)
+
+Controller for the ATM application. Where user input is processed.
+
+@author: Ryan Shoobert
+@since: 2018-08-20 (YYYY-MM-DD)
+@version: 1.0.0
+'''
+
+class ATM_Controller:
+
+    #Constructor
+    def __init__(self):
+        '''
+        '''
---
+++ model/ATM_Model.py
@@ -0,0 +1,16 @@
+'''
+Created on 2018-08-20 (YYYY-MM-DD)
+
+Model for the ATM application. Where the business logic lives.
+
+@author: Ryan Shoobert
+@since: 2018-08-20 (YYYY-MM-DD)
+@version: 1.0.0
+'''
+
+class ATM_Model:
+
+    #Constructor
+    def __init__(self):
+        '''
+        '''
